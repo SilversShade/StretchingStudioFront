15b2570853b69b88e1ebacb8069a56d8f4bb36ef
Alex <shantem965@mail.ru>
2024-04-13 01:06:12 +0500

    added subscription cards display with data fetched from the backend API

--- components/subscription-cards.vue
+++ components/subscription-cards.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="card">
-        <div class="card-title">1 занятие</div>
+        <div class="card-title">Количество занятий: {{ $props.sessionsNum }}</div>
         <div class="card-content">
             <div class="card-label">Стоимость</div>
-            <div class="card-value">660 ₽</div>
+            <div class="card-value">{{ $props.price }} ₽</div>
         </div>
         <button class="card-button"><nuxt-link to="/payment">Купить</nuxt-link></button>
     </div>
 </template>
+
+<script setup>
+const props = defineProps({
+    sessionsNum: Number,
+    price: Number
+})
+</script>
 
 <style scoped src="../assets/css/globals.css"></style>
--- pages/purchase-subscription.vue
+++ pages/purchase-subscription.vue
@@ -4,13 +4,23 @@
             <div class="block-heading">
                 <div class="heading-2">Купить абонемент</div>
             </div>
-            <div class="card-list">
-                <SubscriptionCards />
-                <SubscriptionCards />
-                <SubscriptionCards />
+            <div class="grid grid-cols-3 gap-5"> <!--card-list-->
+                <div v-for="s in subscriptions">
+                    <SubscriptionCards :sessions-num="s.sessionsNum" :price="s.price" />
+                </div>
             </div>
         </div>
     </div>
 </template>
+
+<script setup lang="ts">
+    interface Subscription {
+        id: string
+        sessionsNum: number,
+        price: number
+    }
+
+    const { data: subscriptions } = await useFetch<Subscription[]>('https://localhost:7229/api/v1/available-subscriptions');
+</script>
 
 <style scoped src="../assets/css/globals.css"></style>
