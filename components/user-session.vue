7b03f9cd3cacc83661d58a1bdda25f0e06285822
Alex <shantem965@mail.ru>
2024-04-19 21:45:08 +0500

    now the account page displays user's upcoming sessions

---
+++ components/user-session.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="card">
+        <div class="card-title">{{ sessionType }}</div>
+        <div class="card-content">
+            <div class="card-label pb-2">Дата и время начала</div>
+            <div class="card-value">{{ convertDate(startingDate) }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    sessionType: String,
+    startingDate: String,
+})
+</script>
+
+<style scoped src="../assets/css/globals.css"></style>
--- pages/account.vue
+++ pages/account.vue
@@ -1,13 +1,28 @@
 <template>
-    <div class="page-content">
+    <div class="page-content" v-if="!pending">
         <div class="page-block">
             <div class="block-heading">
-                <div class="heading-2">Ваши абонементы</div>
+                <div class="heading-2">Ваши тренировки</div>
             </div>
             <div class="grid grid-cols-3 gap-5 mr-15">
-                <div v-for="s in subscriptions" :key="s.id">
-                    <UserSubscription :sessions-num="s.sessionsLeft" />
+                <div v-for="s in userData!.sessions" :key="s.id">
+                    <UserSession :session-type="s.session.sessionType.name" :starting-date="s.session.startingDate" />
                 </div>
+            </div>
+            <div v-if="userData!.sessions.length === 0" class="heading-3 text-rose-500">
+                Вы не записаны ни на одну тренировку
+            </div>
+            <div class="block-heading pt-5">
+                <div class="heading-2">Ваши абонементы</div>
+            </div>
+            
+                <div class="grid grid-cols-3 gap-5 mr-15">
+                    <div v-for="s in userData!.subscriptions" :key="s.id">
+                        <UserSubscription :sessions-num="s.sessionsLeft" />
+                    </div>
+                </div>
+            <div v-if="userData!.subscriptions.length === 0" class="heading-3 text-rose-500">
+                У вас нет приобретенных абонементов
             </div>
         </div>
     </div>
@@ -30,18 +45,40 @@ interface Subscription {
     }
 }
 
+interface Session {
+    id: string,
+    session: {
+        sessionType: {
+            name: string,
+        },
+        startingDate: string
+    }
+}
+
 const config = useRuntimeConfig()
-const { data: subscriptions, error } = await useFetch<Subscription[]>(config.public.apiBase + '/api/v1/user-subscriptions', {
-    headers: {
-        "Authorization": "Bearer " + useCookie('accessToken').value
-    },
-    server: false,
-})
+
+const { data: userData, error, pending } = await useAsyncData("userData", async () => {
+    const [subscriptions, sessions] = await Promise.all([
+        $fetch<Subscription[]>(config.public.apiBase + '/api/v1/user-subscriptions', {
+            headers: {
+                "Authorization": "Bearer " + useCookie('accessToken').value
+            }
+        }),
+        $fetch<Session[]>(config.public.apiBase + '/api/v1/user-sessions', {
+            headers: {
+                "Authorization": "Bearer " + useCookie('accessToken').value
+            }
+        })
+    ])
+
+    return { subscriptions, sessions }
+}, {server: false})
+
 if (error.value) {
     const authStore = useAuthStore()
     authStore.resetState()
     useCookie('accessToken').value = null
-    
+
     await navigateTo({ path: '/auth/login' })
 }
 </script>
